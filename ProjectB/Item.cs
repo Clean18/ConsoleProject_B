1198dd590b555305224bd3108ece9668ee56a323
Clean18 <cleanage95@gmail.com>
2025-04-08 10:31:41 +0900

    feat: Item 클래스, IUse 인터페이스 추가
    - Item 추상 클래스 추가
    - IUse 인터페이스 추가 후 Item에 상속

---
+++ ProjectB/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB
+{
+    public abstract class Item : IUse
+    {
+        public string Name { get; set; }    // 이름
+        public string Desc { get; set; }    // 설명
+        public int CurCount { get; set; }   // 현재 개수
+        public int MaxCount { get; set; }   // 전체 개수
+
+        public Item(string name, string desc, int curCount, int maxCount)
+        {
+            Name = name;
+            Desc = desc;
+            CurCount = curCount;
+            MaxCount = maxCount;
+        }
+
+        public abstract void Use();
+    }
+}
